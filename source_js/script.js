b4e7255dd642563b9056ecc37b5220f5922d52d8
Sanjit Dutta <sdlynx@gmail.com>
2016-02-10 22:57:26 -0600

    modals are working with multiple ways to close

--- source_js/script.js
+++ source_js/script.js
@@ -1,7 +1,24 @@
 $(function() {
+
+	// function to open/close modal - called by other functions
+
+	function openModal(id) {
+		$('#' + id).fadeIn();
+	}
+
+	// closes any open modals
+
+	function closeModals() {
+		$('.modal').fadeOut();
+	}
+
+	// de-obfuscate email address on page
+
 	var link = "<n uers=\"znvygb:ebjsbhaqngvba@irevmba.arg\" ery=\"absbyybj\">ebjsbhaqngvba@irevmba.arg</n>".replace(/[a-zA-Z]/g, 
 		function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
 	$('#ROWemail').html(link);
+
+	// carousel - doesn't overanimate with multiple clicks
 
 	var currentPic = 0;
 
@@ -23,5 +40,33 @@ $(function() {
 		}
 		$('#carousel').stop();
 		$('#carousel').animate( {left: ((currentPic * -100) + "%")}, 1000);
+	});
+
+	// 
+
+	$('.modal_link').on("click", function() {
+		var modalToOpen = $(this).attr('id').substring(5);
+		openModal(modalToOpen);
+	});
+
+	$('.close').on("click", function() {
+		closeModals();
+	});
+
+	$(document).on("keyup", function(e) {
+		if(e.keyCode == 27) {
+			closeModals();
+		}
+	});
+
+	// below code taken from http://stackoverflow.com/questions/1403615/use-jquery-to-hide-a-div-when-the-user-clicks-outside-of-it
+
+	$(document).on("mouseup", function (e) {
+	    var container = $(".modal");
+	    if (!container.is(e.target) // if the target of the click isn't the container...
+	        && container.has(e.target).length === 0) // ... nor a descendant of the container
+	    {
+	        closeModals();
+	    }
 	});
 });
